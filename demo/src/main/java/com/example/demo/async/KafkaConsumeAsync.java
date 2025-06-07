a368c05730e8de2ac5bb7a178623efe78d969911
lakshmi <vijayalakshmi.vujjini@gmail.com>
2025-06-07 17:46:43 -0400

    concurrent executions

--- demo/src/main/java/com/example/demo/async/KafkaConsumeAsync.java
+++ demo/src/main/java/com/example/demo/async/KafkaConsumeAsync.java
@@ -1,4 +1,16 @@
 package com.example.demo.async;
 
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.springframework.kafka.annotation.KafkaListener;
+
+import java.util.concurrent.CompletableFuture;
+
 public class KafkaConsumeAsync {
+   @KafkaListener(topics = "test",groupId = "testId")
+    public CompletableFuture<Void> receiveMessage(String message){
+
+        return CompletableFuture.runAsync(()->{ //using run async as we are not returning anything
+            System.out.println("result "+message);
+        });
+    }
 }
--- demo/src/main/java/com/example/demo/async/KafkaProducerAsync.java
+++ demo/src/main/java/com/example/demo/async/KafkaProducerAsync.java
@@ -1,5 +1,26 @@
 package com.example.demo.async;
 
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+
+import java.util.concurrent.CompletableFuture;
+
 public class KafkaProducerAsync {
+    @Autowired
+     KafkaTemplate<String,String> kafkaTemplate;
+public void sendMessage(String topic, String key, String message) {
+    CompletableFuture<SendResult<String, String>> resultCompletableFuture = kafkaTemplate.send(topic, key, message).completable();
+    resultCompletableFuture
+            .thenAcceptAsync(res->{
+             System.out.println("Result"+res.getRecordMetadata());
+                })
+            .exceptionally(ex->{
+                System.out.println("Exception "+ex.getMessage());
+                return null;
+            });
 
 }
+}
+
