9373f496af7fb16bf32a3735cd7d4b405aae5369
lakshmi <vijayalakshmi.vujjini@gmail.com>
2025-06-07 15:30:07 -0400

    concurrent executions

--- demo/src/main/java/com/example/demo/async/executorExmpl.java
+++ demo/src/main/java/com/example/demo/async/executorExmpl.java
@@ -1,4 +1,30 @@
 package com.example.demo.async;
 
+import java.util.concurrent.*;
+
 public class executorExmpl {
+
+    public static void main(String args[]){
+        //create thread pool using executor service
+        ExecutorService executorService= Executors.newFixedThreadPool(1);
+        //create task using Callable- returns value
+        Callable<String> task=()->{
+          return "testing future obj";
+        };
+        // Submit the task to the executor and obtain a Future
+        Future<String> future=executorService.submit(task);
+        //access the future object
+        try{
+           String futureValue= future.get(); //.get is a blocking call
+            System.out.println("Result from future: " + futureValue);
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+// important to close the executor service
+        executorService.shutdown();
+
+    }
 }
--- demo/src/main/java/com/example/demo/async/runnableTask.java
+++ demo/src/main/java/com/example/demo/async/runnableTask.java
@@ -3,7 +3,7 @@ package com.example.demo.async;
 public class runnableTask {
     public static void main(String args[]){
 
-        //create a task using runnable
+        //create a task using runnable- return void
         Runnable task=()->{
             for(int i=0;i<5;i++){
                 System.out.println(i+" "+Thread.currentThread());
