8624c55013645473a83c0c5ca551d50b04625f17
lakshmi <lakshmi19900704@gmail.com>
2025-06-19 00:10:29 -0400

    Error Handling

--- demo/src/main/java/com/example/demo/RestApi/GlobalExceptHand.java
+++ demo/src/main/java/com/example/demo/RestApi/GlobalExceptHand.java
@@ -1,8 +1,17 @@
 package com.example.demo.RestApi;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptHand {
 
+@ExceptionHandler(InvalidInputException.class)
+    public ResponseEntity<ErrorResponse> handleinvalidInput(InvalidInputException i){
+ErrorResponse error=new ErrorResponse("Invalid input",i.getMessage());
+return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+
+}
 }
--- demo/src/main/java/com/example/demo/RestApi/empController.java
+++ demo/src/main/java/com/example/demo/RestApi/empController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.module.ResolutionException;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,8 +20,12 @@ public class empController {
     }
     @GetMapping(path="/getbyid/{id}")
     public ResponseEntity<demoModel> getbyid(@PathVariable("id") int id){
-        Optional<demoModel> demoModel=repo.findById(id);
-        return new ResponseEntity<demoModel>(demoModel.get(),HttpStatus.OK);
+            if (id <= 0) { //below error redirects to globalexception handler and then handled
+                throw new InvalidInputException("Invalid ID: ID must be a positive number");
+            }
+            Optional<demoModel> demoModel = repo.findById(id);
+            return new ResponseEntity<demoModel>(demoModel.get(), HttpStatus.OK);
+
     }
     @PostMapping(path="/postEmp")
     public ResponseEntity<demoModel> saveEmp(@RequestBody demoModel demo){
@@ -31,6 +36,9 @@ public class empController {
     @PutMapping(path="/putemp/{id}")
     public ResponseEntity<demoModel> editEmp(@PathVariable("id") int id, @RequestBody demoModel demo){
        Optional<demoModel> exisiting= repo.findById(id);
+        if (!exisiting.isPresent()) {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
         demoModel d = exisiting.get();
        d.setName(demo.getName());
        d.setMarks(demo.getMarks());
@@ -39,9 +47,15 @@ public class empController {
     }
 
     @DeleteMapping(path="/delete/{id}")
-    public ResponseEntity<demoModel> deletebyID(@PathVariable("id") int id){
+    public ResponseEntity<?> deletebyID(@PathVariable("id") int id){
+        try{
         repo.deleteById(id);
         return new ResponseEntity<>(HttpStatus.OK);
+        }catch(InvalidInputException e){ //below error is handled directly- not recommended
+            ErrorResponse error=new ErrorResponse("invalid input",e.getMessage());
+            return new ResponseEntity<>(error,HttpStatus.BAD_REQUEST);
+
+        }
     }
 
     @DeleteMapping(path = "/deleteall")
