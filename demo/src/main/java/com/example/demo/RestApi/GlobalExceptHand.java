cbe34420b44b8a0fc3ba075e80d845fdd8bd923d
lakshmi <lakshmi19900704@gmail.com>
2025-06-18 23:13:54 -0400

    CRUD

---
+++ demo/src/main/java/com/example/demo/RestApi/GlobalExceptHand.java
@@ -0,0 +1,8 @@
+package com.example.demo.RestApi;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class GlobalExceptHand {
+
+}
---
+++ demo/src/main/java/com/example/demo/RestApi/empController.java
@@ -0,0 +1,43 @@
+package com.example.demo.RestApi;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class empController {
+    @Autowired
+    demoRepo repo;
+    @GetMapping(path = "/getdetails")
+    public ResponseEntity<demoModel> empDetails(){
+        List<demoModel> demoModel=repo.findAll();
+        return new ResponseEntity<demoModel>((demoModel) demoModel,HttpStatus.OK);
+    }
+    @GetMapping(path="/getbyid/{id}")
+    public ResponseEntity<demoModel> getbyid(@PathVariable("id") int id){
+        Optional<demoModel> demoModel=repo.findById(id);
+        return new ResponseEntity<demoModel>(demoModel.get(),HttpStatus.OK);
+    }
+    @PostMapping(path="/postEmp")
+    public ResponseEntity<demoModel> saveEmp(@RequestBody demoModel demo){
+       repo.save(demo);
+        return new ResponseEntity<demoModel>(HttpStatus.CREATED);
+    }
+
+    @PutMapping(path="/putemp/{id}")
+    public ResponseEntity<demoModel> editEmp(@PathVariable("id") int id, @RequestBody demoModel demo){
+       Optional<demoModel> exisiting= repo.findById(id);
+        demoModel d = exisiting.get();
+       d.setName(demo.getName());
+       d.setMarks(demo.getMarks());
+       repo.save(d);
+       return new ResponseEntity<demoModel>(HttpStatus.OK);
+    }
+
+}
+
+
